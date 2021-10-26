b68643aa3ac5c4c04eb9578f881661c3803b2815
greypignest <peterzakanyi@gmail.com>
2021-10-26 23:40:48 +0200

    homework

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
-// function loadEvent () {
-//     const rootElement = document.getElementById("root");
-// }
-
-// window.addEventListener ("load", loadEvent);
+function toggleClicked (event) {
+    document.getElementById("bodyId").classList.toggle("clicked");
+}
 
 function loadEvent() {
     console.log('the page has loaded');
-    
-    function toggleClicked (event) {
-        event.target.classList.toggle("clicked");
-        // document.getElementById("menu-btn").classList.toggle("clicked");
-    };
 
     document.getElementById("menu-btn").addEventListener("click", toggleClicked);
-
-    // document.getElementById("menu-btn").addEventListener("click", function (event) {
-    //     console.log(event.target);
-    //     event.target.classList.toggle("clicked");
-    //     // document.getElementById("menu-btn").classList.toggle("clicked");
-    // });
+    
 }
-
-// függvényt hozzuk ki és kapjon nevet és a működés ne essen szét
-
 window.addEventListener("load", loadEvent);
+
+//bodyra id, click esemény a gombon hívódjon meg, de a class nevet a body kapja (csak ő) és a css uígy működjön -- szelektorok css-ben
+
+/* 
+function toggleClicked (event) {
+    document.getElementById("bodyId").classList.toggle("clicked");
+}
+ */
